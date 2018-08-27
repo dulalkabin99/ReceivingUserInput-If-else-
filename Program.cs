cae1e97f4ff15ec6e4b4ddc5e0b2762e969734bd
dulalkabin99 <dulalkabin99@gmail.com>
2018-08-27 15:38:40 -0400

    Complete

--- Program.cs
+++ Program.cs
@@ -10,6 +10,27 @@ namespace IfElseWhichIsBigger
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please Enter Your Name: ");
+            String nam = Console.ReadLine();
+            Console.WriteLine("Enter 2 Numbers: ");
+            Console.WriteLine("Enter first Number:");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+            int biggerNum = secondNumber;
+
+            if (firstNumber > secondNumber)
+            {
+                biggerNum = firstNumber;
+            }
+            Console.WriteLine("Hello, " + nam + " Thanks For playing");
+            Console.WriteLine("The Bigger Number is: " + biggerNum);
+
+            Console.ReadLine();
+
+
+
+
         }
     }
 }
